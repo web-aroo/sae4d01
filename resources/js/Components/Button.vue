e27f2bd91a36d488ea57a3c68e2491b78f399b7f
davidismeurt <david.ismeurt@protonmail.com>
2023-03-17 10:34:09 +0000

    ✨ Create component Button
    Merge-request: KAYSERSTUHL-ESCAPE-MR-2
    Merged-by: David Ismeurt <david.ismeurt@protonmail.com>

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+	<button class='bg-light-brown px-16 py-4 text-white active:scale-95' :type='type'>
+		<slot></slot>
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+	props: {
+		type: String
+	}
+};
+</script>

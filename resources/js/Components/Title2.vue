70d45fece0dcb6da916c95e10ff5ccdd5426d590
davidismeurt <david.ismeurt@protonmail.com>
2023-03-18 10:41:56 +0100

    ✨ Create component Title2

---
+++ resources/js/Components/Title2.vue
@@ -0,0 +1,14 @@
+<template>
+
+	<div class='h-1 w-16 bg-red-600 m-auto'></div>
+	<h2 class='text-center text-4xl font-serif mt-3'>
+		<slot></slot>
+	</h2>
+
+</template>
+
+<script>
+export default {
+	name: 'Title2'
+};
+</script>

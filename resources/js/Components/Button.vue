80c8654aca21708806974211ddf9a70dc05e5dc4
davidismeurt <david.ismeurt@protonmail.com>
2023-03-21 23:31:58 +0100

    🛠 Adjust Button font

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-	<button class='bg-light-brown px-16 py-4 text-white active:scale-95' :type='type'>
+	<button class='bg-light-brown px-16 py-4 text-white active:scale-95 font-serif text-2xl' :type='type'>
 		<slot></slot>
 	</button>
 </template>

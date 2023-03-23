0a7b08ec80d211b80413d92f461ef19538c18417
leonekmi <me@leonekmi.fr>
2023-03-23 10:16:16 +0000

    ✨ Add <Title3> component
    Co-authored-by: leonekmi <me@leonekmi.fr>
    
    Merge-request: KAYSERSTUHL-ESCAPE-MR-15
    Merged-by: Leon Lozahic <jb@leonekmi.fr>

---
+++ resources/js/Components/Title3.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+	icon: Array,
+});
+</script>
+
+<template>
+	<h3 class="flex gap-3 font-bold items-center">
+		<font-awesome-icon :icon="icon" class="text-light-brown" />
+		<slot></slot>
+	</h3>
+</template>

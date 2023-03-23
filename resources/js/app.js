a676fa2845673a03f6fd600f6c40a319d816593f
zina <zina.boufalaas@uha.fr>
2023-03-23 10:21:52 +0000

    ✨ Add Testimonial

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,7 @@ import {
 	faKey,
 	faUsers,
 	faClock,
+	faStar,
 	faBuildingColumns,
 	faChevronUp,
 } from '@fortawesome/free-solid-svg-icons';
@@ -19,7 +20,7 @@ import { faInstagram, faFacebook, faPaypal } from '@fortawesome/free-brands-svg-
 import I18NextVue from 'i18next-vue';
 import i18next from 'i18next';
 
-library.add(faKey, faUsers, faClock, faInstagram, faFacebook, faPaypal, faBuildingColumns, faChevronUp);
+library.add(faKey, faUsers, faClock, faStar, faInstagram, faFacebook, faPaypal, faBuildingColumns, faChevronUp);
 
 const appName =
 	window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';

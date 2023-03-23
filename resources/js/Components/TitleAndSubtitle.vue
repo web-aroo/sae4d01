6db7a7477119b3dd5a3eb4f6c4cbaabcdc18c1e4
zinabfs <zinaa.bls@gmail.com>
2023-03-23 13:31:37 +0000

    Component Hero
    Co-authored-by: David Ismeurt <david.ismeurt@protonmail.com>
    Co-authored-by: zinabfs <zinaa.bls@gmail.com>
    
    Merge-request: KAYSERSTUHL-ESCAPE-MR-16
    Merged-by: Leon Lozahic <jb@leonekmi.fr>

--- resources/js/Components/TitleAndSubtitle.vue
+++ resources/js/Components/TitleAndSubtitle.vue
@@ -1,11 +1,9 @@
 <template>
-	<div class="p-8 font-serif">
+	<div class="ml-8 font-serif grid gap-4">
 		<Title>{{ title }}</Title>
-		<div class="flex items-center">
-			<div class="h-0.5 w-8 md:w-14 bg-black mr-2"></div>
-			<a href="#" class="text-base md:text-2xl underline">
-				{{ subtitle }}
-			</a>
+		<div class="flex items-center text-white">
+			<div class="h-0.5 w-8 md:w-14 bg-current mr-2"></div>
+			<a href="#" class="text-base md:text-2xl underline">{{ subtitle }}</a>
 		</div>
 	</div>
 </template>

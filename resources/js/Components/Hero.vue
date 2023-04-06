f2a5c4551dd22000f3fa0d1191eb2528ee38dd04
zina <zina.boufalaas@uha.fr>
2023-04-06 11:49:18 +0200

    :bug: Fix Hero color

--- resources/js/Components/Hero.vue
+++ resources/js/Components/Hero.vue
@@ -1,11 +1,14 @@
 <template>
-	<div :style="{'background-image': 'url('+image+')'}" class="w-screen h-[50vh] md:h-screen bg-center before:w-full before:h-[50vh] before:md:h-full before:absolute before:bg-gradient-to-b before:from-black before:via-transparent before:to-very-dark-brown grid items-center">
+	<div
+		:style="{ 'background-image': 'url(' + image + ')' }"
+		class="w-screen h-[50vh] md:h-screen bg-center before:w-full before:h-[50vh] before:md:h-full before:absolute before:bg-gradient-to-b before:from-black before:via-transparent before:to-bg-color grid items-center"
+	>
 		<slot></slot>
 	</div>
 </template>
 
 <script setup>
 defineProps({
-	image: String
-})
+	image: String,
+});
 </script>

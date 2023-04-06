3bddcbf1cd6c4880d3551b38144b92ee5405f881
davidismeurt <david.ismeurt@protonmail.com>
2023-04-06 11:19:18 +0200

    :sparkles: Create PageLayout

--- resources/js/Components/Container.vue
+++
@@ -1,7 +0,0 @@
-<template>
-
-	<div class='max-w-4xl mx-auto sm:px-8'>
-		<slot></slot>
-	</div>
-
-</template>
---
+++ resources/js/Components/layouts/PageLayout.vue
@@ -0,0 +1,16 @@
+<template>
+
+	<div class='min-h-screen grid grid-rows-[1fr_auto] bg-black gap-4'>
+		<Header></Header>
+		<main class='max-w-4xl mx-auto sm:px-8 mt-32'>
+			<slot></slot>
+		</main>
+		<Footer></Footer>
+	</div>
+
+</template>
+
+<script setup>
+import Header from '@/Components/Header.vue';
+import Footer from '@/Components/Footer.vue';
+</script>

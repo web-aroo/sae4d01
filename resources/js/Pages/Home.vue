15554f1894d6f780f8b148697b2bba055bf97a87
davidismeurt <david.ismeurt@protonmail.com>
2023-04-06 13:45:32 +0200

    :sparkles: Create PageLayout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+
+	<PageLayout>
+		<Hero></Hero>
+		<Container>
+			<AdventureCard></AdventureCard>
+		</Container>
+	</PageLayout>
+
+</template>
+<script setup>
+import PageLayout from '@/Components/layouts/PageLayout.vue';
+import AdventureCard from '@/Components/AdventureCard.vue';
+import Container from '@/Components/Container.vue';
+import Hero from '@/Components/Hero.vue';
+</script>
